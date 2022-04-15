a3806df9b9b2e2263db27acfb8d19b04566fc636
axelfiol <axel.fiol97@hotmail.com>
2022-04-15 17:19:29 -0300

    BaSP-W05-2022: finished variables and operators

---
+++ Semana-05/exercises/01_variables_and_operators.js
@@ -0,0 +1,21 @@
+console.log('Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 3er variable');
+
+var ageAxel = 24;
+var ageUriel = 26;
+console.log('edad Axel: ' + ageAxel + ' edad Uriel: ' + ageUriel);
+var sumAges = ageAxel + ageUriel;
+console.log('suma de las edades de Axel y Uriel: ',sumAges);
+
+console.log('Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.');
+
+var string1 = 'estos ejercicios estan ';
+var string2 = 'muy buenos';
+var result = string1 + string2;
+console.log('La concatenacion de los dos string da como resultado: '+ result);
+
+console.log('Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) guardando el resultado de la suma en una 3er variable (utilizar length).')
+
+var string3 = 'auto';
+var string4 = 'blanco';
+var result = string3.length + string4.length;
+console.log('suma de la longitud de los string: '+ result);
